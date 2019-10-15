b726d18870ea47f638bf38faec6313b07a065404
Simon Schubert <sschubert89@gmail.com>
2019-10-15 21:50:42 +0200

    Apply compose development changes

--- app/src/main/java/com/inspiredandroid/braincup/AppTheme.kt
+++ app/src/main/java/com/inspiredandroid/braincup/AppTheme.kt
@@ -1,6 +1,21 @@
+/*
+// * Copyright 2019 The Android Open Source Project
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
 //package androidx.ui.material.studies.rally
 //
-//import androidx.compose.Children
 //import androidx.compose.Composable
 //import androidx.compose.composer
 //import androidx.ui.core.sp
@@ -21,43 +36,43 @@
 //    )
 //    val typography = MaterialTypography(
 //        h1 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w100,
+//            fontWeight = FontWeight.W100,
 //            fontSize = 96.sp),
 //        h2 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w100,
+//            fontWeight = FontWeight.W100,
 //            fontSize = 60.sp),
 //        h3 = TextStyle(fontFamily = FontFamily("Eczar"),
-//            fontWeight = FontWeight.w500,
+//            fontWeight = FontWeight.W500,
 //            fontSize = 48.sp),
 //        h4 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w700,
+//            fontWeight = FontWeight.W700,
 //            fontSize = 34.sp),
 //        h5 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w700,
+//            fontWeight = FontWeight.W700,
 //            fontSize = 24.sp),
 //        h6 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w700,
+//            fontWeight = FontWeight.W700,
 //            fontSize = 20.sp),
 //        subtitle1 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w700,
+//            fontWeight = FontWeight.W700,
 //            fontSize = 16.sp),
 //        subtitle2 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w500,
+//            fontWeight = FontWeight.W500,
 //            fontSize = 14.sp),
 //        body1 = TextStyle(fontFamily = FontFamily("Eczar"),
-//            fontWeight = FontWeight.w700,
+//            fontWeight = FontWeight.W700,
 //            fontSize = 16.sp),
 //        body2 = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w200,
+//            fontWeight = FontWeight.W200,
 //            fontSize = 14.sp),
 //        button = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w800,
+//            fontWeight = FontWeight.W800,
 //            fontSize = 14.sp),
 //        caption = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w500,
+//            fontWeight = FontWeight.W500,
 //            fontSize = 12.sp),
 //        overline = TextStyle(fontFamily = FontFamily("RobotoCondensed"),
-//            fontWeight = FontWeight.w500,
+//            fontWeight = FontWeight.W500,
 //            fontSize = 10.sp)
 //
 //    )
--- app/src/main/java/com/inspiredandroid/braincup/MainActivity.kt
+++ app/src/main/java/com/inspiredandroid/braincup/MainActivity.kt
@@ -1,60 +1,49 @@
-///*
 //package androidx.ui.material.studies
 //
 //import android.app.Activity
 //import android.os.Bundle
-//import android.os.Handler
 //import android.widget.TableRow
 //import androidx.compose.Composable
 //import androidx.compose.State
 //import androidx.compose.composer
 //import androidx.compose.state
 //import androidx.compose.unaryPlus
-//import androidx.ui.core.Duration
 //import androidx.ui.core.Text
-//import androidx.ui.core.TextField
 //import androidx.ui.core.dp
+//import androidx.ui.material.Button
 //import androidx.ui.core.setContent
 //import androidx.ui.core.sp
 //import androidx.ui.graphics.Color
-//import androidx.ui.input.EditorModel
-//import androidx.ui.input.EditorStyle
 //import androidx.ui.layout.Column
 //import androidx.ui.layout.Container
 //import androidx.ui.layout.HeightSpacer
 //import androidx.ui.layout.MainAxisAlignment
-//import androidx.ui.layout.MaxIntrinsicHeight
-//import androidx.ui.layout.MinIntrinsicWidth
 //import androidx.ui.layout.Padding
-//import androidx.ui.layout.Row
 //import androidx.ui.layout.Table
 //import androidx.ui.layout.TableColumnWidth
 //import androidx.ui.material.Button
 //import androidx.ui.material.studies.Scaffold
 //import androidx.ui.material.studies.rally.AppTheme
-//import androidx.ui.material.studies.rally.RallyAccountsCard
-//import androidx.ui.material.studies.rally.RallyAlertCard
-//import androidx.ui.material.studies.rally.RallyBillsCard
 //import androidx.ui.material.themeTextStyle
-//import androidx.ui.temputils.delay
 //import androidx.ui.text.ParagraphStyle
 //import androidx.ui.text.TextStyle
 //import androidx.ui.text.style.TextAlign
 //import androidx.ui.text.style.TextDirection
-//import java.lang.System.currentTimeMillis
 //import android.os.AsyncTask
 //import android.widget.FrameLayout
 //import androidx.ui.core.WithDensity
-//import androidx.ui.core.vectorgraphics.DrawVector
-//import androidx.ui.core.vectorgraphics.compat.vectorResource
 //import androidx.ui.foundation.SimpleImage
-//import androidx.ui.painting.Image
-//import androidx.ui.painting.imageFromResource
+//import androidx.ui.graphics.vector.DrawVector
+//import androidx.ui.layout.CrossAxisAlignment
+//import androidx.ui.layout.Row
+//import androidx.ui.material.TextButtonStyle
+//import androidx.ui.res.vectorResource
 //import com.inspiredandroid.braincup.app.AppController
 //import com.inspiredandroid.braincup.app.AppInterface
 //import com.inspiredandroid.braincup.app.AppState
+//import com.inspiredandroid.braincup.games.ChainCalculationGame
 //import com.inspiredandroid.braincup.games.ColorConfusionGame
-//import com.inspiredandroid.braincup.games.Game
+//import com.inspiredandroid.braincup.games.GameType
 //import com.inspiredandroid.braincup.games.MentalCalculationGame
 //import com.inspiredandroid.braincup.games.SherlockCalculationGame
 //import com.inspiredandroid.braincup.games.getName
@@ -83,24 +72,30 @@
 //    @Composable
 //    fun BaseApp(children: @Composable() () -> Unit) {
 //        AppTheme {
-//            Column(mainAxisAlignment = MainAxisAlignment.Center) {
+//            Column(mainAxisAlignment = MainAxisAlignment.Center, crossAxisAlignment =
+//            CrossAxisAlignment.Center) {
 //                children()
 //            }
 //        }
 //    }
 //
-//    override fun showMainMenu(title: String, description: String, games: List<Game.Type>, callback:
-//        (Game.Type) -> Unit) {
+//    override fun showMainMenu(
+//        title: String,
+//        description: String,
+//        games: List<GameType>,
+//        instructions: (GameType) -> Unit,
+//        score: (GameType) -> Unit
+//    ) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text(title, +themeTextStyle { h5 })
+//                Text(title, style = +themeTextStyle { h5 })
 //                HeightSpacer(8.dp)
-//                Text(description, +themeTextStyle { subtitle1 })
+//                Text(description, style = +themeTextStyle { subtitle1 })
 //                HeightSpacer(16.dp)
 //                games.forEach {
 //                    HeightSpacer(16.dp)
-//                    Button(it.getName(), {
-//                        callback(it)
+//                    Button(text = it.getName(), onClick = {
+//                        instructions(it)
 //                    })
 //                }
 //                val vectorAsset = +vectorResource(R.drawable.ic_waiting)
@@ -111,32 +106,34 @@
 //        }
 //    }
 //
-//    override fun showInstructions(title: String, description: String, start: (Long) -> Unit) {
+//    override fun showInstructions(title: String, description: String, start: () -> Unit) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text(title, +themeTextStyle { h6 })
+//                Text(title, style = +themeTextStyle { h6 })
 //                Padding(16.dp, 16.dp, 16.dp, 24.dp) {
-//                    Text(
-//                        description,
-//                        +themeTextStyle {
+//                    Text(description,
+//                        style = +themeTextStyle {
 //                            subtitle1
-//                        }, ParagraphStyle(textAlign = TextAlign.Center)
+//                        }, paragraphStyle = ParagraphStyle(textAlign = TextAlign.Center)
 //                    )
 //                }
 //                Button("Start", {
-//                    start(currentTimeMillis())
+//                    start()
 //                })
 //            }
 //        }
 //    }
 //
-//    override fun showMentalCalculation(game: MentalCalculationGame, answer: (String) -> Unit, next:
-//        (Long) -> Unit) {
+//    override fun showMentalCalculation(
+//        game: MentalCalculationGame,
+//        answer: (String) -> Unit,
+//        next: () -> Unit
+//    ) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text(game.calculation, +themeTextStyle { h3 })
+//                Text(game.calculation, style = +themeTextStyle { h3 })
 //                NumberPad(onInputChange = {
-//                    if(it.length == game.number.toString().length) {
+//                    if(game.getNumberLength() == it.length) {
 //                        answer(it)
 //                        DelayedTask().execute(next)
 //                    }
@@ -145,16 +142,22 @@
 //        }
 //    }
 //
-//    override fun showColorConfusion(game: ColorConfusionGame, answer: (String) -> Unit, next: (Long)
-//    -> Unit) {
+//    override fun showColorConfusion(
+//        game: ColorConfusionGame,
+//        answer: (String) -> Unit,
+//        next: () -> Unit
+//    ) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text("${game.shapePoints} = ${game.answerShape.getName()}", +themeTextStyle { h5 })
-//                Text("${game.colorPoints} = ${game.answerColor.getName()}", TextStyle(fontSize = 24.sp, color = game
+//                Text("${game.shapePoints} = ${game.answerShape.getName()}", style=
+//                +themeTextStyle { h5 })
+//                Text("${game.colorPoints} = ${game.answerColor.getName()}", style = TextStyle
+//                        (fontSize =
+//                24.sp, color = game
 //                    .stringColor
 //                    .getComposeColor())
 //                )
-//                Text(game.displayedShape.getChar(), TextStyle(fontSize = 96.sp, color = game
+//                Text(game.displayedShape.getChar(), style = TextStyle(fontSize = 96.sp, color = game
 //                    .displayedColor
 //                    .getComposeColor())
 //                )
@@ -168,12 +171,15 @@
 //        }
 //    }
 //
-//    override fun showSherlockCalculation(game: SherlockCalculationGame, answer: (String) -> Unit,
-//                                         next: (Long) -> Unit) {
+//    override fun showSherlockCalculation(
+//        game: SherlockCalculationGame,
+//        answer: (String) -> Unit,
+//        next: () -> Unit
+//    ) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text("Goal: ${game.result}", +themeTextStyle { h3 })
-//                Text("Numbers: ${game.numbers.joinToString()}", +themeTextStyle { h5 })
+//                Text("Goal: ${game.result}", style = +themeTextStyle { h3 })
+//                Text("Numbers: ${game.getNumbersString()}", style = +themeTextStyle { h5 })
 //                NumberPad(true, onInputChange = {
 //                    if(game.isCorrect(it)) {
 //                        answer(it)
@@ -189,6 +195,37 @@
 //        }
 //    }
 //
+//    override fun showChainCalculation(
+//        game: ChainCalculationGame,
+//        answer: (String) -> Unit,
+//        next: () -> Unit
+//    ) {
+//        frameLayout.setContent {
+//            BaseApp {
+//                Text(game.calculation, style = +themeTextStyle { h4 })
+//                NumberPad(false, onInputChange = {
+//                    if(game.isCorrect(it)) {
+//                        answer(it)
+//                        DelayedTask().execute(next)
+//                    }
+//                })
+//                HeightSpacer(32.dp)
+//                Button("Give up", onClick = {
+//                    answer("")
+//                    DelayedTask().execute(next)
+//                })
+//            }
+//        }
+//    }
+//
+//    override fun showScoreboard(
+//        game: GameType,
+//        highscore: Int,
+//        scores: List<Pair<String, List<Int>>>
+//    ) {
+//
+//    }
+//
 //    override fun showCorrectAnswerFeedback() {
 //        frameLayout.setContent {
 //            BaseApp {
@@ -200,7 +237,7 @@
 //        }
 //    }
 //
-//    override fun showWrongAnswerFeedback() {
+//    override fun showWrongAnswerFeedback(solution: String) {
 //        frameLayout.setContent {
 //            BaseApp {
 //                val vectorAsset = +vectorResource(R.drawable.ic_searching)
@@ -211,10 +248,19 @@
 //        }
 //    }
 //
-//    override fun showFinishFeedback(rank: String, plays: Int, random: () -> Unit) {
+//    override fun showFinishFeedback(
+//        rank: String,
+//        newHighscore: Boolean,
+//        plays: Int,
+//        random: () -> Unit
+//    ) {
 //        frameLayout.setContent {
 //            BaseApp {
-//                Text(rank, +themeTextStyle { h6 })
+//                Text("Score: $rank", style = +themeTextStyle { h3 })
+//                HeightSpacer(16.dp)
+//                if(newHighscore) {
+//                    // Text("New highscore", style = +themeTextStyle { h6 })
+//                }
 //                Button("Next game", {
 //                    random()
 //                })
@@ -225,19 +271,20 @@
 //    @Composable
 //    fun NumberPad(showOperators: Boolean = false, onInputChange: (String) -> Unit) {
 //        val input = +state { "" }
-//        Padding(16.dp, 16.dp, 16.dp, 16.dp) {
-//            Text(
-//                input.value, style = TextStyle(color = Color.Black, fontSize = 32.sp)
-//            )
-//        }
-//        Container(height = 50.dp) {
+//        Row(crossAxisAlignment = CrossAxisAlignment.Center) {
+//            Padding(16.dp, 16.dp, 16.dp, 16.dp) {
+//                Text(
+//                    input.value, style = TextStyle(color = Color.Black, fontSize = 32.sp)
+//                )
+//            }
 //            if (input.value.isNotEmpty()) {
 //                Button("◄", {
 //                    input.value = input.value.substring(0, input.value.lastIndex)
 //                })
 //            }
 //        }
-//        Table(columnCount = if(showOperators) { 4 } else { 3 }, columnWidth = { TableColumnWidth.Inflexible.Wrap }) {
+//        Table(columns = if(showOperators) { 4 } else { 3 }, columnWidth = {
+//            TableColumnWidth.Wrap }) {
 //            tableRow {
 //                NumberPadButton("7", input, onInputChange)
 //                NumberPadButton("8", input, onInputChange)
@@ -291,19 +338,18 @@
 //        }
 //    }
 //
-//    */
-///**
+//    /**
 //     * Temporary solution
-//     *//*
+//     */
 //
 //    private fun Shape.getChar(): String {
-////        return when(this) {
-////            Shape.SQUARE -> "■"
-////            Shape.TRIANGLE -> "▲"
-////            Shape.CIRCLE -> "◯"
-////            Shape.HEART -> "♥"
-////        }
-////    }
+//        return when(this) {
+//            Shape.SQUARE -> "■"
+//            Shape.TRIANGLE -> "▲"
+//            Shape.CIRCLE -> "◯"
+//            Shape.HEART -> "♥"
+//        }
+//    }
 //
 //    private fun com.inspiredandroid.braincup.games.tools.Color.getComposeColor(): androidx.ui.graphics.Color {
 //        return when(this) {
@@ -314,19 +360,19 @@
 //        }
 //    }
 //
-//    */
-///**
-//     * Temporary solution until coroutines will work
-//     *//*
 //
-//    private inner class DelayedTask : AsyncTask<(Long) -> Unit, Int, (Long) -> Unit>() {
-//        override fun doInBackground(vararg next: (Long) -> Unit): (Long) -> Unit {
+//    /**
+//     * Temporary solution until coroutines will work
+//     */
+//
+//    private inner class DelayedTask : AsyncTask<() -> Unit, Int, () -> Unit>() {
+//        override fun doInBackground(vararg next: () -> Unit): () -> Unit {
 //            Thread.sleep(1000)
 //            return next[0]
 //        }
 //
-//        override fun onPostExecute(result: (Long) -> Unit) {
-//            result(currentTimeMillis())
+//        override fun onPostExecute(result: () -> Unit) {
+//            result()
 //        }
 //    }
-//}*/
+//}
