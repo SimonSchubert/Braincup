e62ec33d37764400cda8137dd8ab28fe61f139cf
Simon Schubert <sschubert89@gmail.com>
2019-12-01 20:08:41 +0100

    Use constants in build src

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,8 +1,8 @@
 object Config {
-    val applicationId = "com.inspiredandroid.braincup"
-    val versionCode = 4
-    val versionName = "1.2.0"
-    val minSDK = 22
-    val targetSDK = 29
-    val instrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    const val applicationId = "com.inspiredandroid.braincup"
+    const val versionCode = 5
+    const val versionName = "1.2.1"
+    const val minSDK = 22
+    const val targetSDK = 29
+    const val instrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
 }
--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,16 +1,16 @@
 object Lib {
     object Versions {
-        val kotlin = "1.3.61"
-        val gradleBuildTools = "4.0.0-alpha04"
-        val gradleSpawn = "0.8.2"
-        val androidxUi = "0.1.0-dev02"
-        val androidxCoreKtx = "1.1.0"
-        val androidxPreferenceKtx = "1.1.0"
-        val multiplatformSettings = "0.4.1"
-        val materialDesign = "1.0.0"
-        val versionsPlugin = "0.27.0"
-        val ktor = "1.2.6"
-        val appenginePluginVersion = "1.3.5"
-        val appengineVersion = "1.9.77"
+        const val kotlin = "1.3.61"
+        const val gradleBuildTools = "4.0.0-alpha04"
+        const val gradleSpawn = "0.8.2"
+        const val androidxUi = "0.1.0-dev02"
+        const val androidxCoreKtx = "1.1.0"
+        const val androidxPreferenceKtx = "1.1.0"
+        const val multiplatformSettings = "0.4.1"
+        const val materialDesign = "1.0.0"
+        const val versionsPlugin = "0.27.0"
+        const val ktor = "1.2.6"
+        const val appenginePluginVersion = "1.3.5"
+        const val appengineVersion = "1.9.77"
     }
 }
