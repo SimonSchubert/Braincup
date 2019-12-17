2a0c322aa3512a8b745bb97385cedc3fc199d4c7
Simon Schubert <sschubert89@gmail.com>
2019-12-17 20:19:05 +0100

    Upgrade gradle/multiplatformsettings

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,12 +1,12 @@
 object Lib {
     object Versions {
         const val kotlin = "1.3.61"
-        const val gradleBuildTools = "4.0.0-alpha05"
+        const val gradleBuildTools = "4.0.0-alpha06"
         const val gradleSpawn = "0.8.2"
         const val androidxUi = "0.1.0-dev03"
         const val androidxCoreKtx = "1.1.0"
         const val androidxPreferenceKtx = "1.1.0"
-        const val multiplatformSettings = "0.4.1"
+        const val multiplatformSettings = "0.5"
         const val materialDesign = "1.0.0"
         const val versionsPlugin = "0.27.0"
         const val ktor = "1.2.6"
