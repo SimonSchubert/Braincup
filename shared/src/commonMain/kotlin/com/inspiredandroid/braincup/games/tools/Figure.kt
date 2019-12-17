68d729c18fea31755a29a948c0f894df79dd4669
Simon Schubert <sschubert89@gmail.com>
2019-12-17 20:13:11 +0100

    Add rotation to figures

---
+++ shared/src/commonMain/kotlin/com/inspiredandroid/braincup/games/tools/Figure.kt
@@ -0,0 +1,13 @@
+package com.inspiredandroid.braincup.games.tools
+
+class Figure(var shape: Shape, var color: Color, var rotation: Int = 0) {
+    fun getRotationString(): String {
+        return when (rotation) {
+            0 -> "up"
+            90 -> "right"
+            180 -> "down"
+            270 -> "left"
+            else -> ""
+        }
+    }
+}
