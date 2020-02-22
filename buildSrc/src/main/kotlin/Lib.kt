9902f1104c58a03ffff0be75193caed414d7852f
Simon Schubert <sschubert89@gmail.com>
2020-02-22 10:33:16 +0100

    Upgrade ktor/version plugin/gradle/kotlinxhtml/cloudstore/appengine/materialdesign

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -7,10 +7,10 @@ object Lib {
         const val androidxCoreKtx = "1.1.0"
         const val androidxPreferenceKtx = "1.1.0"
         const val multiplatformSettings = "0.5"
-        const val materialDesign = "1.0.0"
-        const val versionsPlugin = "0.27.0"
-        const val ktor = "1.3.0"
+        const val materialDesign = "1.1.0"
+        const val versionsPlugin = "0.28.0"
+        const val ktor = "1.3.1"
         const val appenginePluginVersion = "1.3.5"
-        const val appengineVersion = "1.9.77"
+        const val appengineVersion = "1.9.78"
     }
 }
