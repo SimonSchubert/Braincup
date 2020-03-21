3ee65606aa223c411cefed3ec513713ed9eed99c
Simon Schubert <sschubert89@gmail.com>
2020-03-21 15:06:56 +0100

    Upgade to Kotlin 1.3.70/ktor/multiplatformsettings/gradlebuildtools/serialization/klock

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,16 +1,16 @@
 object Lib {
     object Versions {
-        const val kotlin = "1.3.61"
-        const val gradleBuildTools = "4.0.0-alpha09"
+        const val kotlin = "1.3.70"
+        const val gradleBuildTools = "4.1.0-alpha03"
         const val gradleSpawn = "0.8.2"
         const val androidxUi = "0.1.0-dev05"
         const val androidxCoreKtx = "1.2.0"
         const val androidxPreferenceKtx = "1.1.0"
-        const val multiplatformSettings = "0.5"
+        const val multiplatformSettings = "0.5.1"
         const val materialDesign = "1.1.0"
         const val versionsPlugin = "0.28.0"
-        const val ktor = "1.3.1"
+        const val ktor = "1.3.2"
         const val appenginePluginVersion = "1.3.5"
-        const val appengineVersion = "1.9.78"
+        const val appengineVersion = "1.9.79"
     }
 }
